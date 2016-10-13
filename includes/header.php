2bbba2400f376070a1db68427e6d639b22f19ffa
Johannes Neumeier <hello@johannesneumeier.com>
2016-10-13 11:29:27 +0300

    fixed wordpress debug complaints about undefined indexes

--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,27 @@
 <header id="fontsampler-menu">
 	<ul>
-		<li <?php if ( empty( $_GET['subpage'] ) || in_array( $_GET['subpage'], array(
+		<li <?php if ( isset( $_GET['subpage'] ) && ( empty( $_GET['subpage'] ) || in_array( $_GET['subpage'], array(
 				'edit',
 				'create',
 		) )
-		) : echo 'class="current"'; endif; ?>>
+		) ) : echo 'class="current"'; endif; ?>>
 			<a href="?page=fontsampler">Font samplers</a>
 		</li>
-		<li <?php if ( 'fonts' == $_GET['subpage'] || in_array( $_GET['subpage'], array(
+		<li <?php if ( isset( $_GET['subpage'] ) && ( 'fonts' == $_GET['subpage'] || in_array( $_GET['subpage'], array(
 				'font_edit',
 				'font_create',
 		) )
-		) : echo 'class="current"'; endif; ?>>
+		) ) : echo 'class="current"'; endif; ?>>
 			<a href="?page=fontsampler&amp;subpage=fonts">Fonts &amp; files</a>
 		</li>
-		<li <?php if ( 'settings' == $_GET['subpage'] || in_array( $_GET['subpage'], array(
+		<li <?php if ( isset( $_GET['subpage'] ) && ( 'settings' == $_GET['subpage'] || in_array( $_GET['subpage'], array(
 				'font_edit',
 				'font_create',
 		) )
-		) : echo 'class="current"'; endif; ?>>
+		) ) : echo 'class="current"'; endif; ?>>
 			<a href="?page=fontsampler&amp;subpage=settings">Settings</a>
 		</li>
-		<li <?php if ( 'about' == $_GET['subpage'] ) : echo ' class="current"'; endif; ?>>
+		<li <?php if ( isset( $_GET['subpage'] ) && 'about' == $_GET['subpage'] ) : echo ' class="current"'; endif; ?>>
 			<a href="?page=fontsampler&amp;subpage=about">About</a>
 		</li>
 	</ul>
