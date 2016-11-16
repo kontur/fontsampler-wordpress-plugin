7b845d012ef690e63081c2d09d82dcbafb5c1302
Johannes Neumeier <hello@johannesneumeier.com>
2016-11-16 21:17:44 +0200

    class based uninstall

--- uninstall.php
+++ uninstall.php
@@ -4,30 +4,17 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
+require_once( 'FontsamplerPlugin.php' );
+
+// Convenience subclasses instantiated within the FontsamplerPlugin class
+require_once( 'FontsamplerDatabase.php' );
+require_once( 'FontsamplerFormhandler.php' );
+require_once( 'FontsamplerHelpers.php' );
+require_once( 'FontsamplerPagination.php' );
+require_once( 'FontsamplerMessages.php' );
+
 // TODO figure out why I can't embed and call Fontsampler->uninstall() instead
 global $wpdb;
+$f = new FontsamplerPlugin( $wpdb );
 
-$sql = "DROP TABLE IF EXISTS " . $wpdb->prefix . "fontsampler_sets";
-$wpdb->query( $sql );
-
-$sql = "DROP TABLE IF EXISTS " . $wpdb->prefix . "fontsampler_fonts";
-$wpdb->query( $sql );
-
-$sql = "DROP TABLE IF EXISTS " . $wpdb->prefix . "fontsampler_sets_x_fonts";
-$wpdb->query( $sql );
-
-$sql = "DROP TABLE IF EXISTS " . $wpdb->prefix . "fontsampler_settings";
-$wpdb->query( $sql );
-
-
-// finally, remove fontsampler settings from wp_options
-
-if ( get_option( 'fontsampler_db_version' ) ) {
-	delete_option( 'fontsampler_db_version' );
-}
-
-if ( get_option( 'fontsampler_hide_legacy_formats' ) ) {
-	delete_options( 'fontsampler_hide_legacy_formats' );
-}
-
-?>
+$f->fontsampler_uninstall();
