1913af320012996e8b75a6aba0286fa342acb263
Johannes Neumeier <hello@johannesneumeier.com>
2017-02-24 16:16:20 +0200

    version bump to 0.1.6

--- fontsampler.php
+++ fontsampler.php
@@ -3,7 +3,7 @@
 Plugin Name: Fontsampler
 Plugin URI:  https://fontsampler.johannesneumeier.com
 Description: Create interactive webfont previews via shortcodes
-Version:     0.1.5
+Version:     0.1.6
 Author:      Johannes Neumeier
 Author URI:  http://johannesneumeier.com
 Copyright:   Copyright 2016-2017 Johannes Neumeier
