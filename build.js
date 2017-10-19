6221b872d8a8bd481a4e92f68dce5364db63cd88
Johannes Neumeier <hello@johannesneumeier.com>
2017-10-19 10:39:11 +0300

    replaced build.js deps with bower paths

--- build.js
+++ build.js
@@ -1,18 +1,18 @@
 ({
-    baseUrl: "./js",
+    // baseUrl: "./js",
     paths: {
-        'opentype': 'libs/opentype.js/dist/opentype',
-        'Atem-CPS-whitelisting': 'libs/Atem-CPS-whitelisting/lib',
-        'Atem-Errors': 'libs/Atem-Errors/lib',
-        'Atem-Math-Tools': 'libs/Atem-Math-Tools/lib',
-        'Atem-Pen-Case': 'libs/Atem-Pen-Case/lib',
-        'require/text': 'libs/requirejs-text/text',
-        'specimenTools': 'libs/specimen-tools/lib',
-        'rangeslider': 'libs/rangeslider.js/dist/rangeslider',
-        'selectric': 'libs/jquery-selectric/public/jquery.selectric',
-        'requireLib': 'libs/requirejs/require'
+        'opentype': 'bower_components/opentype.js/dist/opentype',
+        'Atem-CPS-whitelisting': 'bower_components/Atem-CPS-whitelisting/lib',
+        'Atem-Errors': 'bower_components/Atem-Errors/lib',
+        'Atem-Math-Tools': 'bower_components/Atem-Math-Tools/lib',
+        'Atem-Pen-Case': 'bower_components/Atem-Pen-Case/lib',
+        'require/text': 'bower_components/requirejs-text/text',
+        'specimenTools': 'bower_components/specimen-tools/lib',
+        'rangeslider': 'bower_components/rangeslider.js/dist/rangeslider',
+        'selectric': 'bower_components/jquery-selectric/public/jquery.selectric',
+        'requireLib': 'bower_components/requirejs/require'
     },
-    name: "main",
+    name: "js/main",
     out: "js/bundle.js",
     include: "requireLib"
 })
